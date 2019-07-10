4756ba0f021c9f49ce3caa8e95014a1fa58f25a8
huwenxiang <282543820@qq.com>
2019-07-10 10:04:08 +0800

    加入GCN

---
+++ GCN/model/gcn.py
@@ -0,0 +1,195 @@
+"""
+GCN model for relation extraction.
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+
+from model.tree import Tree, head_to_tree, tree_to_adj
+from utils import constant, torch_utils
+
+class GCNClassifier(nn.Module):
+    """ A wrapper classifier for GCNRelationModel. """
+    def __init__(self, opt, emb_matrix=None):
+        super().__init__()
+        self.gcn_model = GCNRelationModel(opt, emb_matrix=emb_matrix)
+        in_dim = opt['hidden_dim']
+        self.classifier = nn.Linear(in_dim, opt['num_class'])
+        self.opt = opt
+
+    def conv_l2(self):
+        return self.gcn_model.gcn.conv_l2()
+
+    def forward(self, inputs):
+        outputs, pooling_output = self.gcn_model(inputs)
+        logits = self.classifier(outputs) # output 就是在pooling_output的基础上加入了 subj和obj 的 信息。pooling_output
+        return logits, pooling_output
+
+class GCNRelationModel(nn.Module):
+    def __init__(self, opt, emb_matrix=None):
+        super().__init__()
+        self.opt = opt
+        self.emb_matrix = emb_matrix # 词向量
+
+        # create embedding layers
+        self.emb = nn.Embedding(opt['vocab_size'], opt['emb_dim'], padding_idx=constant.PAD_ID)
+        self.pos_emb = nn.Embedding(len(constant.POS_TO_ID), opt['pos_dim']) if opt['pos_dim'] > 0 else None
+        self.ner_emb = nn.Embedding(len(constant.NER_TO_ID), opt['ner_dim']) if opt['ner_dim'] > 0 else None
+        embeddings = (self.emb, self.pos_emb, self.ner_emb)
+        self.init_embeddings()
+
+        # gcn layer
+        self.gcn = GCN(opt, embeddings, opt['hidden_dim'], opt['num_layers'])
+
+        # output mlp layers
+        in_dim = opt['hidden_dim']*3
+        layers = [nn.Linear(in_dim, opt['hidden_dim']), nn.ReLU()]
+        for _ in range(self.opt['mlp_layers']-1):
+            layers += [nn.Linear(opt['hidden_dim'], opt['hidden_dim']), nn.ReLU()]
+        self.out_mlp = nn.Sequential(*layers)
+
+    def init_embeddings(self):
+        if self.emb_matrix is None:
+            self.emb.weight.data[1:,:].uniform_(-1.0, 1.0)
+        else:
+            self.emb_matrix = torch.from_numpy(self.emb_matrix)
+            self.emb.weight.data.copy_(self.emb_matrix)
+        # decide finetuning
+        if self.opt['topn'] <= 0:
+            print("Do not finetune word embedding layer.")
+            self.emb.weight.requires_grad = False
+        elif self.opt['topn'] < self.opt['vocab_size']:
+            print("Finetune top {} word embeddings.".format(self.opt['topn']))
+            self.emb.weight.register_hook(lambda x: \
+                    torch_utils.keep_partial_grad(x, self.opt['topn']))
+        else:
+            print("Finetune all embeddings.")
+
+    def forward(self, inputs):
+        words, masks, pos, ner, deprel, head, subj_pos, obj_pos, subj_type, obj_type = inputs # unpack
+        l = (masks.data.cpu().numpy() == 0).astype(np.int64).sum(1)
+        maxlen = max(l) #
+
+        def inputs_to_tree_reps(head, words, l, prune, subj_pos, obj_pos):
+            head, words, subj_pos, obj_pos = head.cpu().numpy(), words.cpu().numpy(), subj_pos.cpu().numpy(), obj_pos.cpu().numpy()
+            trees = [head_to_tree(head[i], words[i], l[i], prune, subj_pos[i], obj_pos[i]) for i in range(len(l))] # 这里是循环每个样本
+            adj = [tree_to_adj(maxlen, tree, directed=False, self_loop=False).reshape(1, maxlen, maxlen) for tree in trees] # 这里应该是创建临街矩阵
+            adj = np.concatenate(adj, axis=0)# 将list 转成矩阵
+            adj = torch.from_numpy(adj) # 这里生成torch 对象
+            return Variable(adj.cuda()) if self.opt['cuda'] else Variable(adj)
+
+        adj = inputs_to_tree_reps(head.data, words.data, l, self.opt['prune_k'], subj_pos.data, obj_pos.data) # 经过建立一大推tree ，最后返回的[batch_size,maxlen,maxlen] 的邻接矩阵
+        h, pool_mask = self.gcn(adj, inputs)
+        
+        # pooling
+        subj_mask, obj_mask = subj_pos.eq(0).eq(0).unsqueeze(2), obj_pos.eq(0).eq(0).unsqueeze(2) # invert mask [20,60,1] 通过两次eq0 subj 的位置变成了0
+        pool_type = self.opt['pooling']
+        h_out = pool(h, pool_mask, type=pool_type)
+        subj_out = pool(h, subj_mask, type=pool_type)
+        obj_out = pool(h, obj_mask, type=pool_type)
+        outputs = torch.cat([h_out, subj_out, obj_out], dim=1)
+        outputs = self.out_mlp(outputs)
+        return outputs, h_out
+
+class GCN(nn.Module):
+    """ A GCN/Contextualized GCN module operated on dependency graphs. """
+    def __init__(self, opt, embeddings, mem_dim, num_layers):
+        super(GCN, self).__init__()
+        self.opt = opt
+        self.layers = num_layers
+        self.use_cuda = opt['cuda']
+        self.mem_dim = mem_dim# 神经元个数
+        self.in_dim = opt['emb_dim'] + opt['pos_dim'] + opt['ner_dim']
+
+        self.emb, self.pos_emb, self.ner_emb = embeddings
+
+        # rnn layer
+        if self.opt.get('rnn', False):
+            input_size = self.in_dim
+            self.rnn = nn.LSTM(input_size, opt['rnn_hidden'], opt['rnn_layers'], batch_first=True, \
+                    dropout=opt['rnn_dropout'], bidirectional=True)
+            self.in_dim = opt['rnn_hidden'] * 2
+            self.rnn_drop = nn.Dropout(opt['rnn_dropout']) # use on last layer output
+
+        self.in_drop = nn.Dropout(opt['input_dropout'])
+        self.gcn_drop = nn.Dropout(opt['gcn_dropout'])
+
+        # gcn layer
+        self.W = nn.ModuleList()
+
+        for layer in range(self.layers):
+            input_dim = self.in_dim if layer == 0 else self.mem_dim
+            self.W.append(nn.Linear(input_dim, self.mem_dim))
+
+    def conv_l2(self):
+        conv_weights = []
+        for w in self.W:
+            conv_weights += [w.weight, w.bias]
+        return sum([x.pow(2).sum() for x in conv_weights])
+
+    def encode_with_rnn(self, rnn_inputs, masks, batch_size):
+        seq_lens = list(masks.data.eq(constant.PAD_ID).long().sum(1).squeeze())
+        h0, c0 = rnn_zero_state(batch_size, self.opt['rnn_hidden'], self.opt['rnn_layers'])
+        rnn_inputs = nn.utils.rnn.pack_padded_sequence(rnn_inputs, seq_lens, batch_first=True)
+        rnn_outputs, (ht, ct) = self.rnn(rnn_inputs, (h0, c0))
+        rnn_outputs, _ = nn.utils.rnn.pad_packed_sequence(rnn_outputs, batch_first=True)
+        return rnn_outputs
+
+    def forward(self, adj, inputs):
+        words, masks, pos, ner, deprel, head, subj_pos, obj_pos, subj_type, obj_type = inputs # unpack
+        word_embs = self.emb(words)
+        embs = [word_embs]
+        if self.opt['pos_dim'] > 0:
+            embs += [self.pos_emb(pos)]
+        if self.opt['ner_dim'] > 0:
+            embs += [self.ner_emb(ner)]
+        embs = torch.cat(embs, dim=2)# 维度是[batch_size,maxlen,360]
+        embs = self.in_drop(embs) # 加入dropout
+
+        # rnn layer
+        if self.opt.get('rnn', False):
+            gcn_inputs = self.rnn_drop(self.encode_with_rnn(embs, masks, words.size()[0]))
+        else:
+            gcn_inputs = embs
+        
+        # gcn layer
+        denom = adj.sum(2).unsqueeze(2) + 1# [batch_size,maxlen,?]+1 这里是用来求平均的，但 后面会加一次自己本身(self loop) ，所以加1
+        mask = (adj.sum(2) + adj.sum(1)).eq(0).unsqueeze(2)# (adj.sum(2) + adj.sum(1)) 是【batch_size,maxlen】 代表 第I行和第I列元素相加。#  对于有向图 需要adj.sum(2) + adj.sum(1) ，无向图的话就adj.sum(2) 就够了，因为之前做了转置相加
+        # zero out adj for ablation
+        if self.opt.get('no_adj', False):
+            adj = torch.zeros_like(adj)
+
+        for l in range(self.layers):
+            Ax = adj.bmm(gcn_inputs) # 这里就相当于做了聚合
+            AxW = self.W[l](Ax)# 这里就是一个线性变换
+            AxW = AxW + self.W[l](gcn_inputs) # self loop # 这里加入自己的节点信息到h中，孤立的节点就只有自己
+            AxW = AxW / denom # 这里相当于求平均
+
+            gAxW = F.relu(AxW)
+            gcn_inputs = self.gcn_drop(gAxW) if l < self.layers - 1 else gAxW # 只有在第一层加入输出 dropout
+
+        return gcn_inputs, mask
+
+def pool(h, mask, type='max'):
+    if type == 'max':
+        h = h.masked_fill(mask, -constant.INFINITY_NUMBER)# 这里将mask为1的地方所在的行 变为 固定值。
+        return torch.max(h, 1)[0] # 返回每一列的最大值，
+    elif type == 'avg':
+        h = h.masked_fill(mask, 0)
+        return h.sum(1) / (mask.size(1) - mask.float().sum(1))
+    else:
+        h = h.masked_fill(mask, 0)
+        return h.sum(1)
+
+def rnn_zero_state(batch_size, hidden_dim, num_layers, bidirectional=True, use_cuda=True):
+    total_layers = num_layers * 2 if bidirectional else num_layers
+    state_shape = (total_layers, batch_size, hidden_dim)
+    h0 = c0 = Variable(torch.zeros(*state_shape), requires_grad=False)
+    if use_cuda:
+        return h0.cuda(), c0.cuda()
+    else:
+        return h0, c0
+
